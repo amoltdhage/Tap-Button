5b7a4d2c0130897241a24143c2d81afae4c1082d
Felix ITs 02 <felixits02@felixs-macbook-pro-2.local>
2016-05-19 12:50:59 +0530

    Initial Commit

---
+++ ATDTapbutton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATDTapbutton
+//
+//  Created by Felix ITs 02 on 19/05/16.
+//  Copyright © 2016 Felix ITs 02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ATDTapbutton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ATDTapbutton
+//
+//  Created by Felix ITs 02 on 19/05/16.
+//  Copyright © 2016 Felix ITs 02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
